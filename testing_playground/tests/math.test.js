a434d2ecc7222a8c10d49cc6d5216917d3f331f8
Paul Wechuli <wechuli@Pauls-MacBook-Pro.local>
2019-08-14 12:20:19 +0300

    testing async code

--- testing_playground/tests/math.test.js
+++ testing_playground/tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require("../dummyFunctions/math");
+const {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit
+} = require("../dummyFunctions/math");
 
 test("should calculate total with tip ", () => {
   const total = calculateTip(10, 0.3);
@@ -9,3 +13,18 @@ test("Should calculate total with default tip", () => {
   const total = calculateTip(10);
   expect(total).toBe(12.5);
 });
+
+test("Should convert 32F to C", () => {
+  result = fahrenheitToCelsius(32);
+  expect(result).toBe(0);
+});
+
+test("Should convert 0 c to 32 f", () => {
+  result = celsiusToFahrenheit(0);
+  expect(result).toBe(32);
+});
+
+
+test('Async test demo',()=>{
+  expect(1).toBe(2);
+})
